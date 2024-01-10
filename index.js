26af3640aa63724be9799f0755ba8ac230ef0504
Vladyslav <kramarenko.creator@gmail.com>
2024-01-10 19:51:57 +0200

    intercepted errors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 import {registerValidation} from './validations/auth.js'
+import UserModel from './models/User.js'
 
 mongoose.connect(
-	'mongodb+srv://kramarenko:LM2CYJ54Kj5LHAut@cluster0.g5xktzq.mongodb.net/')
+	'mongodb+srv://kramarenko:LM2CYJ54Kj5LHAut@cluster0.g5xktzq.mongodb.net/blog')
 .then(() => console.log('DB OK'))
 .catch((err) => console.log('DB ERROR', err));
 
@@ -13,15 +15,33 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-	const errors = validationResult(req);
-	if(!errors.isEmpty()) {
-		return res.status(400).json(errors.array());
+app.post('/auth/register', registerValidation, async(req, res) => {
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json(errors.array());
+		}
+
+		const password = req.body.password;
+		const salt = await bcrypt.genSalt(10);
+		const passwordHash = await bcrypt.hash(password, salt);
+
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash,
+		});
+
+		const user = await doc.save();
+
+		res.json(user);
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Failed to register'
+		})
 	}
-	
-	res.json({
-		succes: true,
-	})
 });
 
 app.listen(4444, err => {
