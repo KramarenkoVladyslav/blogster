6d6de7e843d24c10ce9b5880cb516b0379a62db3
Vladyslav <kramarenko.creator@gmail.com>
2024-01-12 18:41:24 +0200

    fixed UserController and rename User.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { validationResult } from 'express-validator';
-import UserModel from '../models/user.js';
+import UserModel from './models/User.js';
 
 export const register = async (req, res) => {
 	try {
