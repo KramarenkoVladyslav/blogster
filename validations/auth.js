5448bd63d6dac945fe0776df47d2a04c0b10e5e6
Vladyslav <kramarenko.creator@gmail.com>
2024-01-12 18:44:35 +0200

    Deleted validations/auth.js

--- validations/auth.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator'
-
-export const registerValidation = [
-	body('email', 'Invalid email format').isEmail(),
-	body('password', 'Password must contain at least 5 characters').isLength({min: 5,}),
-	body('fullName', 'Fullname is requaired and must be a valid name').isLength({ min: 3 }),
-	body('avatarUrl', 'Incorrect avatar link').optional().isURL(),
-];
